9b38c1662cfcb48257049617bfcb1e3057bbe673
Rickytovfer1 <150245106+Rickytovfer1@users.noreply.github.com>
2025-11-27 13:38:24 +0100

    Cambio de de nombre base datos

--- alimentos2020/app/Config.php
+++ alimentos2020/app/Config.php
@@ -3,7 +3,7 @@
  class Config
  {
      static public $mvc_bd_hostname = "mysql-grupo6daw2025.alwaysdata.net";
-     static public $mvc_bd_nombre   = "alimentos2020";
+     static public $mvc_bd_nombre   = "grupo6daw2025_alimentos62025";
      static public $mvc_bd_usuario  = "grupo6daw2025_alimentos62025";
      static public $mvc_bd_clave    = "grupo6daw2025";
      static public $mvc_vis_css     = "estilo.css";
