4eae193fb51469eceb2ab4eff0fe68af5c138106
Rickytovfer1 <150245106+Rickytovfer1@users.noreply.github.com>
2025-11-27 13:40:31 +0100

    Cambio en el usuario

--- alimentos2020/app/Config.php
+++ alimentos2020/app/Config.php
@@ -4,7 +4,7 @@
  {
      static public $mvc_bd_hostname = "mysql-grupo6daw2025.alwaysdata.net";
      static public $mvc_bd_nombre   = "grupo6daw2025_alimentos62025";
-     static public $mvc_bd_usuario  = "grupo6daw2025_alimentos62025";
+     static public $mvc_bd_usuario  = "443049";
      static public $mvc_bd_clave    = "grupo6daw2025";
      static public $mvc_vis_css     = "estilo.css";
  }
