c484ebc94ce815823a11239b7707f5c907f5805d
Rickytovfer1 <150245106+Rickytovfer1@users.noreply.github.com>
2025-11-27 13:13:18 +0100

    Update Config.php

--- alimentos2020/app/Config.php
+++ alimentos2020/app/Config.php
@@ -2,7 +2,7 @@
 
  class Config
  {
-     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_hostname = "mysql-grupo6daw2025.alwaysdata.net";
      static public $mvc_bd_nombre   = "alimentos2020";
      static public $mvc_bd_usuario  = "grupo6daw2025_alimentos62025";
      static public $mvc_bd_clave    = "grupo6daw2025";
